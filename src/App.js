3b66a717bc89e2bb4dfbcbd20b554cb2ae2a5065
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-05-03 18:16:27 -0400

    Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import About from "./componets/About"
+import About from "./componets/About";
+import Nav from "./componets/Nav";
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
